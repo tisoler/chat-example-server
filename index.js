9911f8d970feb6e3b8f2d10aea9da397f74a34eb
Diego Díaz Barroso <diegodiazbarroso@Diegos-MacBook-Air.local>
2025-08-15 10:59:23 -0300

    Cmabios

--- index.js
+++ index.js
@@ -3,16 +3,28 @@ const { WebSocketServer } = require('ws')
 
 const sockserver = new WebSocketServer({ port: 8080 })
 sockserver.on('connection', (ws, req) => {
-  console.log('New client connected!')
+	const usuario = req?.url?.replace('/', '')
+  	console.log('New client connected!')
  	ws.send('connection established')
  	ws.on('close', () => console.log('Client has disconnected!'))
+	ws.usuario = usuario; // identificar usuario
  	ws.on('message', data => {
-   	sockserver.clients.forEach(client => {
-			const usuario = req?.url?.replace('/', '')
-			console.log(`distributing message - ${usuario}: ${data}`)
-			client.send(`${usuario}: ${data}`)
-  	})
+		const body = JSON.parse(data);
+		if (body.tipo === 'publico') {
+			sockserver.clients.forEach(client => {
+				console.log(`distributing message - ${usuario}: ${body.mensaje}`)
+				client.send(`${usuario}: ${body.mensaje}`)
+			})
+		} else {
+			for (const client of sockserver.clients) {
+				if (client.usuario === body.destinatario) {
+					client.send(`${usuario}: ${body.mensaje}`)
+					break;
+				}
+			}
+		}
  	})
+
 	ws.onerror = function () {
 		console.log('websocket error')
 	}
